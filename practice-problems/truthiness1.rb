0b00728398a7e86c174fabded946939814b7da6b
n8faz <119473201+n8faz@users.noreply.github.com>
2023-10-23 12:00:07 -0700

    Add truthiness 1 problem

---
+++ practice-problems/truthiness1.rb
@@ -0,0 +1,9 @@
+# What does the following code return? What does it output? Why? What concept does it demonstrate?
+
+a = "Hello"
+
+if a
+  puts "Hello is truthy"
+else
+  puts "Hello is falsey"
+end
